159bddeeb2988fcf7e38bd9d98d38a16194650d5
Jenna <88835851+itcodegirl@users.noreply.github.com>
2024-09-05 00:31:04 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import Home from './components/Home';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
+import Newsletter from './components/Newsletter';
+import Footer from './components/Footer';
 
 function App() {
 	return (
@@ -10,6 +14,10 @@ function App() {
 			<Header />
 			<Home />
 			<AboutMe />
+			<Blog />
+			<Contact />
+			<Newsletter />
+			<Footer />
 		</div>
 	);
 };
