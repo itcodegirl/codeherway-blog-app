4d8fa50656c96ba6c9c522d37382061f901bf2f8
Jenna <88835851+itcodegirl@users.noreply.github.com>
2024-09-04 22:38:23 -0500

    Header Update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from './components/Home';
 import Header from './components/Header';
+import AboutMe from './components/AboutMe';
 
 function App() {
 	return (
 		<div>
 			<Header />
 			<Home />
+			<AboutMe />
 		</div>
 	);
 };
