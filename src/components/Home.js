b685edd5e4edbbe9b7519b1d87c1c4a00f8be319
Jenna <88835851+itcodegirl@users.noreply.github.com>
2024-09-08 12:09:19 -0500

    Update Blog Home Photo

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,15 +22,20 @@ function Home() {
 	return (
 		<section id="home" className="p-5 text-center">
 			{/* Background image - ensure it's in the public folder */}
+			<div id="box" ref={videoRef}></div> {/* Ref for video handling */}
 			<section id='container1'>
 				<div className="container">
-					<div id="box" ref={videoRef}></div> {/* Ref for video handling */}
-					<h1 className="display-4">Welcome to CodeHerWay</h1>
-					<p className="lead">A blog for web development enthusiasts, featuring tutorials, insights, and personal stories.</p>
+					<img src="/Images/About_Image/codeherway-font-4.png" alt="CodeHerWay Logo" height="300" width="auto" />
+					<br />
+					<br />
+					<img src="/Image_Logos/About_Blog_Banner.png" alt="CodeHerWay Logo" height="400" width="auto" font-color='white' />
+					<br />
+					<br />
+
 					<a href="#blog" className="btn btn-primary btn-lg">Check Out My Latest Posts</a>
 				</div>
-			</section>
 
+			</section>
 			{/* About Section */}
 			<section id="about" className="p-5">
 				<div className="p-2 bg-dark text-light" id='container2'>
@@ -51,45 +56,45 @@ function Home() {
 				<Carousel>
 					<Carousel.Item>
 						<img
-							className="d-block w-100"
+							className="d-block"
 							src="/Images/Images/laptop-women-tech.png"
-							alt="Women in tech working on laptop"
+							alt="Women in tech working on laptop" id='carousel-image'
 						/>
 						<Carousel.Caption>
-							<h3>First Slide Label</h3>
-							<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+							<h3 id='carousel-caption'>Bridging the Gender Gap in Tech</h3>
+							<p>Women remain underrepresented in tech roles, particularly in areas like design, web development, and software engineering. </p>
 						</Carousel.Caption>
 					</Carousel.Item>
 					<Carousel.Item>
 						<img
-							className="d-block w-100"
+							className="d-block"
 							src="/Images/Women_Tech/women-tech-image-1.png"
-							alt="Coding on a laptop"
+							alt="Coding on a laptop" id='carousel-image'
 						/>
-						<Carousel.Caption>
-							<h3>Second Slide Label</h3>
+						<Carousel.Caption class='carousel-caption'>
+							<h3 id='carousel-caption'>Second Slide Label</h3>
 							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 						</Carousel.Caption>
 					</Carousel.Item>
 					<Carousel.Item>
 						<img
-							className="d-block w-100"
+							className="d-block"
 							src="/Images/Women_Tech/women-tech-image-5.png"
-							alt="Web development project showcase"
+							alt="Web development project showcase" id='carousel-image'
 						/>
 						<Carousel.Caption>
-							<h3>Third Slide Label</h3>
+							<h3 id='carousel-caption'>Third Slide Label</h3>
 							<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
 						</Carousel.Caption>
 					</Carousel.Item>
 					<Carousel.Item>
 						<img
-							className="d-block w-100"
+							className="d-block"
 							src="/Images/Women_Tech/women-tech-image-7.png"
-							alt="Web development project showcase"
+							alt="Web development project showcase" id='carousel-image'
 						/>
 						<Carousel.Caption>
-							<h3>Fourth Slide Label</h3>
+							<h3 id='carousel-caption'>Fourth Slide Label</h3>
 							<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
 						</Carousel.Caption>
 					</Carousel.Item>
